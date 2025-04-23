10e3212a84e3bbdf3075785b6400132b7c4d31a6
dima0073231 <147907949+dima0073231@users.noreply.github.com>
2025-04-23 13:59:56 +0300

    add burger

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
     const modal = document.getElementById('cartModal');
     modal.classList.toggle('active');
   }
+  function toggleCart() {
+    document.getElementById('cartModal').classList.toggle('active');
+  }
+
+  function toggleMenu() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+  }
+  
